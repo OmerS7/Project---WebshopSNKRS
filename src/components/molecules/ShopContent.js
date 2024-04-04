c0219688e0d72dbd641e37bc3463eb217faf6681
Omer Seker <otseker@hotmail.com>
2024-04-04 10:24:50 +0200

    removed comments and took out code for new component

--- src/components/molecules/ShopContent.js
+++ src/components/molecules/ShopContent.js
@@ -1,80 +1,38 @@
 import React, { useEffect } from 'react';
-// import Button from '../atoms/Buttons/Button'; // Importeer de benodigde componenten
-import PngImage from '../atoms/Images/Image';
-import Text from '../atoms/Texts/Text';
-// import { faBagShopping,  } from '@fortawesome/free-solid-svg-icons';
-// import ShoppingCartContext from '../atoms/ShoppingCard';
+import ProductCard from './ProductCard'; // Importeer de ProductCard-component
 import products from '../atoms/products/Products';
 import './_shopContent.styles.scss';
-import { Link } from 'react-router-dom';
 
 const ShopContent = () => {
-  // const { addToCart } = useContext(ShoppingCartContext);
-  // const [showAlert, setShowAlert] = useState(null);
-  // const timeoutRef = useRef(null); 
+  const chunkArray = (arr, size) => {
+    const chunkedArr = [];
+    for (let i = 0; i < arr.length; i += size) {
+      chunkedArr.push(arr.slice(i, i + size));
+    }
+    return chunkedArr;
+  };
 
-  // const handleAddToCart = (item) => {
-  //   addToCart(item);
-  //   setShowAlert(item.imgSrc);
-  //   if (timeoutRef.current) {
-  //     clearTimeout(timeoutRef.current);
-  //   }
-  //   timeoutRef.current = setTimeout(() => {
-  //     setShowAlert(null);
-  //     timeoutRef.current = null;
-  //   }, 2000);
-    
-  //   setTimeout(() => {
-  //       document.querySelector('.alert').classList.add('slideOut');
-  //     }, 1500);
-  // };
-  
-    const chunkArray = (arr, size) => {
-      const chunkedArr = [];
-      for (let i = 0; i < arr.length; i += size) {
-        chunkedArr.push(arr.slice(i, i + size));
-      }
-      return chunkedArr;
-    };
-  
-    const rowsOfProducts = chunkArray(products, 3);
+  // Pas de grootte aan op basis van het schermformaat
+  const rowsOfProducts = window.innerWidth <= 768 ? chunkArray(products, 2) : chunkArray(products, 3);
 
-    useEffect(() => {
-      window.scrollTo(0, 0);
-    }, []);
-  
-    return (
-      <div className='contentFullshop'>
-        {/* {showAlert && (
-                    <div className="alert">
-                        <img src={showAlert} alt="Product added to cart" className="alert-image" />
-                        <p>Added to cart</p>
-                     </div>
-            )} */}
-        <div className='contentShop'>  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className='contentFullshop'>
+      <div className='contentShop'>  
         <h1>ALL SNKRS</h1>
         {rowsOfProducts.map((row, index) => (
           <div className="row" key={index}>
             {row.map(product => (
-           <div className={`card10 ${product.id === 18 ? 'special-product' : ''}`} key={product.id}>
-              <div className="content10">
-                <div className="cardContent10">
-                        <Text content={product.name} />
-                        <div className='price10'>
-                        <Text content={`€${product.price.toFixed(2)}`} />
-                        </div>
-                        <Link to={`/productdetail/${product.id}`}>
-                          <PngImage src={product.imgSrc} alt={product.name} className="shoe10" />
-                        </Link>
-                    </div>
-                </div>
-              </div>
+              <ProductCard key={product.id} product={product} />
             ))}
           </div>
         ))}
-        </div>  
-      </div>
-    );
-  };
-  
-  export default ShopContent;
+      </div>  
+    </div>
+  );
+};
+
+export default ShopContent;
