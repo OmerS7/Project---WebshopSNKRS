4de6b7fb5ded82e1592a427f873bf729b5e3e7a6
Omer Seker <otseker@hotmail.com>
2024-04-04 10:26:05 +0200

    added useState and new function for vw

--- src/components/molecules/Header.js
+++ src/components/molecules/Header.js
@@ -1,13 +1,27 @@
-import React, { useEffect } from "react";
-import HamburterTrigger from "../atoms/nav/MenuTrigger";
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import HamburgerTrigger from "../atoms/nav/MenuTrigger";
 import products from "../atoms/products/Products";
-// import SearchButton from "../atoms/Buttons/SearchButtton"; 
 import SearchButton from "../atoms/Buttons/SearchBar";
 import Logo from "../atoms/nav/Logo";
 import ShoppingBagButton from "../atoms/nav/Shoppingbag";
 import './_header.styles.scss';
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 768);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth < 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     useEffect(() => {
         let lastScrollTop = 0;
         const navbar = document.querySelector('.navBar');
@@ -26,7 +40,6 @@ const Header = () => {
 
         window.addEventListener('scroll', handleScroll);
 
-       
         return () => {
             window.removeEventListener('scroll', handleScroll); 
         };
@@ -37,19 +50,31 @@ const Header = () => {
             <div className="leftSideNav">
                 <ShoppingBagButton />
                 <div className="searchButton">
-                    <SearchButton products={products} /> 
+                    <SearchButton products={products} />
                 </div>
             </div>
             <div className="midSecNav">
                 <Logo />
             </div>
             <div className="rightSecNav">
-                <HamburterTrigger />
+                {isMobile ? (
+                    <HamburgerTrigger />
+                ) : (
+                    <div className="menuItems">
+                    <ul>
+                        <li>
+                            <Link to='/shop'>Shop</Link>
+                        </li>
+                        <li>
+                            <Link to='/about'>Info</Link>
+                        </li>
+                    </ul>
+                </div>
+
+                )}
             </div>
         </div>
     );
 }
 
-
 export default Header;
-
