8340b37d45eae94f91de7fd048b213603f8d8788
Omer Seker <otseker@hotmail.com>
2024-07-19 13:40:16 +0200

    removed and added new information to infopage

--- src/components/pages/InfoPage.js
+++ src/components/pages/InfoPage.js
@@ -3,10 +3,9 @@ import Text from '../atoms/Texts/Text';
 import Footer from "../molecules/Footer";
 import Header from "..//molecules/Header";
 import './_infopage.styles.scss';
+import Gif from "../atoms/Gifs/Gif";
 import PngImage from "../atoms/Images/Image";
-import NikeTerra from '../styles/img/NikeTerra.png';
-import NikeAir from '../styles/img/NikeAirBlue.png';
-
+import KobeBryant from '../styles/img/kobeBryant.png';
 
 function InfoPage() {
 
@@ -15,18 +14,18 @@ function InfoPage() {
       }, []);
 
     return (
-        <div>
+        <div className="contentInfo">
             <Header />
             <div className="content3">
                 <div className="contentFirst">
                     <Text content="This webpage has been created by a web developer for demonstrative purposes, serving as a project aimed at honing skills in React development. Please note that this site is purely fictitious and is not intended for actual commercial transactions 
                                 Visit the nike store www.nike.com to buy yourself shoes. " />
-                    <PngImage src= { NikeTerra } alt={ "Nike logo"} className="logo9"/>
+                    <PngImage src= { KobeBryant } alt={ "Nike logo"} className="logo9"/>
                 </div>
             </div>
             <div className="content4">
              <div className="contentSecond">
-                    <PngImage src= { NikeAir } alt={ "Nike logo"} className="logo9"/>
+                <Gif className="logo9"/>
                     <Text content="If you want to see other React project like this,
                                 Visit my githubprofile www.github.com or look
                                 at my online portfolio www.omer.com. " />
